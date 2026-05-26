ed34ad66cdec2a9e84eacbaab9b2e996964fbe6b
chihirofujima <chihiro.fujima@gmail.com>
2026-05-26 20:30:57 +0900

    feat: CSVファイル名をfe-sikenのURLパスに統一しシードスクリプトを簡略化

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,8 @@
 require 'csv'
 
-# r05 → "05_menjo", r06 → "06_haru" or "06_aki" など
-# CSVにexplanation_urlが入っていない場合の自動生成ルール
-YEAR_PATH = {
-  "r05" => "05_menjo"
-  # 今後追加する場合はここに追記
-  # "r06" => "06_haru",
-}.freeze
-
 Dir[Rails.root.join('db/csv/*.csv')].each do |file|
-  year = File.basename(file, '.csv')
-  url_path = YEAR_PATH[year] || year  # 対応表になければファイル名をそのまま使う
-  Rails.logger.info "Importing #{File.basename(file)}..."
+  url_path = File.basename(file, '.csv') 
+  Rails.logger.info "Importing #{url_path}..."
 
   CSV.foreach(file, headers: true, encoding: 'BOM|UTF-8') do |row|
     number = row['number'].to_i
@@ -29,4 +20,6 @@ Dir[Rails.root.join('db/csv/*.csv')].each do |file|
       q.save!
     end
   end
+
+  puts "#{url_path}: インポート完了（合計 #{Question.count} 件）"
 end
