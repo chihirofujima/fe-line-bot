fca26f94b3f054ce1e992b94ba83cd6d024741cd
chihirofujima <chihiro.fujima@gmail.com>
2026-06-11 20:31:48 +0900

    配信設定画面にバリデーションとトースト通知を追加

--- app/models/delivery_setting.rb
+++ app/models/delivery_setting.rb
@@ -6,4 +6,14 @@ class DeliverySetting < ApplicationRecord
   validates :frequency, presence: true
   validates :delivery_time_1, presence: true
   validates :user_id, uniqueness: true
+  validate :delivery_times_must_differ
+
+  private
+
+  def delivery_times_must_differ
+    return if delivery_time_2.blank?
+    if delivery_time_1 == delivery_time_2
+      errors.add(:deliverry_time_2, "は配信時間①と異なる時刻を設定してください")
+    end
+  end
 end
