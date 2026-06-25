a56d5ca98c0c7f25bbc6baf5b990647fc9bd04ba
chihirofujima <chihiro.fujima@gmail.com>
2026-06-25 16:06:41 +0900

    設定LIFFの配色変更とガイドページのフォーマット統一
    - 設定画面のアクセントカラー変更

--- app/controllers/liff/help_controller.rb
+++ app/controllers/liff/help_controller.rb
@@ -1,6 +1,9 @@
 module Liff
   class HelpController < ApplicationController
+    layout "public"
+
     def index
+      @liff_settings_url = "https://liff.line.me/#{ENV['LIFF_ID_SETTINGS']}"
     end
   end
 end
